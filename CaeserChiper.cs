812164ba04d02a649f5f58e3ccc5b1dfb0004412
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2022-10-10 15:15:13 +0100

    CC

--- CaeserChiper.cs
+++ CaeserChiper.cs
@@ -4,6 +4,31 @@ namespace ALevelTheory
 {
     class Program
     {
+
+        private static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - offset) % 26) + offset);
+        }
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
